087a98fa947223ac06bf3355f0088e6efd805d22
AzStowe <aaron.z.stowe@gmail.com>
2019-05-07 14:16:52 -0500

    adding some basic functions for the fire button and parameters for the guessing function

--- battleship/js/main.js
+++ battleship/js/main.js
@@ -4,13 +4,51 @@ let model = {
     shipLength: 3,
     boardSize: 7,
     shipsSunk: 0,
+    
+    ships: [
+    {locations: [0, 0, 0,], hits: ['', '', '']},
+    {locations: [0, 0, 0,], hits: ['', '', '']},
+    {locations: [0, 0, 0,], hits: ['', '', '']},
+],
+
+fire: function(guess) {
+
+  for(var i = 0; i < this.numShips; i++) {
+    var ship = this.ships[i];
+    var index = ship.locations.indexOf(guess);
+
+    // check if a ship location has already been hit
+    if ( ship.hits[index] === "hit" ) {
+      view.displayMessage("Oops, you already hit that location");
+      return true;
+    } else if ( index >= 0 ) {
+      ship.hits[index] = "hit";
+      view.displayHit(guess);
+      view.displayMessage("HIT!");
+
+      if ( this.isSunk(ship) ) {
+        view.displayMessage("You sank my battleship!");
+        this.shipsSunk++;
+      }
+      return true;
+    }
+  }
+  view.displayMiss(guess);
+  view.displayMessage("You Missed");
+  return false;
+},
+
+isSunk: function(ship) {
+  for (var i = 0; i < this.shipLength; i++) {
+    if (ship.hits[i] !== "hit") {
+      return false;
+    }
+  }
+  return true;
+},
 }
 
-ships: [
-    {locations: [0, 0, 0,], hits: ['', '', '']},
-    {locations: [0, 0, 0,], hits: ['', '', '']},
-    {locations: [0, 0, 0,], hits: ['', '', '']},
-];
+
 
 
 /*----- app's state (variables) -----*/
@@ -20,26 +58,48 @@ let winner, board, turn;
 
 
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', handleFire);
-
+function handleFireButton() {
+	let guessInput = document.getElementById("guessInput");
+	let guess = guessInput.value.toUpperCase();
+	controller.processGuess(guess);
+	guessInput.value = "";
+}
 /*----- functions -----*/
 
 init();
 
 //click function to register the input
-function handleFire {
+function getGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
+	if (guess === null || guess.length !== 2) {
+		alert("Please enter a letter and a number on the board.");
+	} else {
+		let firstChar = guess.charAt(0);
+		let row = alphabet.indexOf(firstChar);
+		let column = guess.charAt(1);
+		if (isNaN(row) || isNaN(column)) {
+			alert("That isn't on the board.");
+		} else if (row < 0 || row >= 6 || column <= 0 || column >= 6) {
+				alert("That's off the board!");
+		} else {
+			return row + column;
+		}
+	}
+	return null;
 }
 
-function init() {
-    board = [
-      [0, 0, 0, 0, 0, 0],  // column 1
-      [0, 0, 0, 0, 0, 0],  // column 2
-      [0, 0, 0, 0, 0, 0],  // column 3
-      [0, 0, 0, 0, 0, 0],  // column 4
-      [0, 0, 0, 0, 0, 0],  // column 5
-      [0, 0, 0, 0, 0, 0],  // column 6
-      [0, 0, 0, 0, 0, 0],  // column 7
-    ];
-    render();
-  }
+let view = {
+	displayMessage: function(msg) {
+		let messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		let cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		let cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
